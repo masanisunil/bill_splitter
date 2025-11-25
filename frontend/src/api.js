67221fb5c0bf108f91e29244301b6e40740bd3fa
masanisunil <sunil.m0711@gmail.com>
2025-11-25 17:16:51 +0530

    Add gunicorn to requirements

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
 const API = axios.create({
-  baseURL: 
+  baseURL:
     process.env.NODE_ENV === "production"
       ? "https://bill-splitter-backend-dsj9.onrender.com/api/"
-      : "http://localhost:8000/api/",
+      : "http://127.0.0.1:8000/api/",
 });
+
 
 export default API;
