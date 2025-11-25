1a6a3cb1cf4966db55f5f64a7f72ad59234a082c
masanisunil <sunil.m0711@gmail.com>
2025-11-25 14:30:40 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://bill-splitter-backend-dsj9.onrender.com/api/",
+
 });
 
 export default API;
